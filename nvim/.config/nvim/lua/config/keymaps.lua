3d92b39a0087c7552c65cccfd1c215cea092492f
Eduard Pelesh <edpelesh@gmail.com>
2025-05-12 17:41:04 +0200

    update keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,6 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find project files" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 
 vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", { desc = "Neotree filesystem reveal toggle" })
