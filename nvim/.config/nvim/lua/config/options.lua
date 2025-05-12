4d37a70b20430ef7bed34dfe3bd0b9364f32dad3
Eduard Pelesh <edpelesh@gmail.com>
2025-05-12 17:56:23 +0200

    add extra options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,16 +3,22 @@ local opt = vim.opt
 opt.number = true
 opt.relativenumber = true
 
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.softtabstop = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
 opt.expandtab = false
 opt.autoindent = true
+opt.smartindent = true
 
+opt.wrap = true
+
+opt.hlsearch = false
+opt.incsearch = true
 opt.ignorecase = true
 opt.smartcase = true
 
 opt.cursorline = true
+opt.scrolloff = 8
 opt.signcolumn = "yes"
 
 opt.backspace = "indent,eol,start"
@@ -23,8 +29,12 @@ opt.splitright = true
 opt.splitbelow = true
 
 opt.iskeyword:append("-")
+opt.isfname:append("@-@")
 
 opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
 opt.list = true
 opt.listchars = {
@@ -35,6 +45,9 @@ opt.listchars = {
 	precedes = '⟨',
 	nbsp = '␣',
 }
+
+opt.termguicolors = true
+opt.updatetime = 50
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
