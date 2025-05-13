c23d2059e749469f32cfefcb8cf84dfda8430b5e
Eduard Pelesh <edpelesh@gmail.com>
2025-05-13 17:29:49 +0200

    add blame styling

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -5,7 +5,15 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup()
+			require("gitsigns").setup({
+				preview_config = {
+					style = "minimal",
+					relative = "cursor",
+					border = "single",
+					row = 0,
+					col = 1,
+				},
+			})
 
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
